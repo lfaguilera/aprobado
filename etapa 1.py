6f9f981c0dd10e3c4cb004beeb85fdd046ab9e50
josecerda1 <62906370+josecerda1@users.noreply.github.com>
2021-10-25 19:13:36 -0300

    Update etapa 1.py

--- etapa 1.py
+++ etapa 1.py
@@ -1,15 +1,30 @@
 def Memotest_e1():
-    fichas={1:'D',2:'D',3:'s',4:'s'}
-    posiciones=[1,2,3,4]
-    print('Fichas y Posiciones: ',posiciones)
+    soniguales=False
+    while soniguales==False:
+
+        fichas={1:'D',2:'D',3:'s',4:'s'}
+        posiciones=[1,2,3,4]
+        print('Fichas y Posiciones: ',posiciones)
     
-    opcion_1 = int(input('1er Posicion:'))
-    posiciones[opcion_1-1] = fichas[opcion_1] 
-    print(posiciones)
+        opcion_1 = int(input('1er Posicion:'))
+        posiciones[opcion_1-1] = fichas[opcion_1] 
+        print(posiciones)
     
-    opcion_2 = int(input('2do Posicion:')) 
-    posiciones[opcion_2-1] = fichas[opcion_2]
-   
-    print(posiciones)
-Memotest_e1()
-# hasta aca llege. no se como hacer el ciclo para que vuelva si no son gemelas.
+        opcion_2 = int(input('2do Posicion:')) 
+        posiciones[opcion_2-1] = fichas[opcion_2]
+    
+        print(posiciones)
+    
+        if posiciones[opcion_2-1]==posiciones[opcion_1-1]:
+            soniguales=True
+    
+    return soniguales
+
+def main ():
+    soniguales=False
+    Memotest_e1()
+    print("GANARON")
+    
+    
+    
+main()
