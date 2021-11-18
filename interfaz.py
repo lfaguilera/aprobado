27292d11ecb80dec22258daad6601fadac1f2714
lfaguilera <lfaguilera@fi.uba.ar>
2021-11-18 14:08:04 -0300

    Squashed commit of the following:
    commit 7ec576a8c83ea75b26ccfe4f389829818502cc86
    Author: lfaguilera <67346702+lfaguilera@users.noreply.github.com>
    Date:   Thu Nov 18 00:32:54 2021 -0300
    
        correccion 2
    
    commit 8b07524c6fa5d5e0df5b4c22e7fbcd3c8468ad68
    Author: lfaguilera <67346702+lfaguilera@users.noreply.github.com>
    Date:   Thu Nov 18 00:32:22 2021 -0300
    
        correccion
    
    commit c11bbc806bb0700764e6d8478c0617576251597d
    Author: lfaguilera <67346702+lfaguilera@users.noreply.github.com>
    Date:   Wed Nov 17 23:38:33 2021 -0300
    
        agregue los turnos y los puntos
    
    commit dc543f90bc585c17e0a17c940222c411e0b09f72
    Author: josecerda1 <62906370+josecerda1@users.noreply.github.com>
    Date:   Wed Nov 17 23:01:46 2021 -0300
    
        ajustes adicionales y comentarios
    
    commit 09aa68f8da259eb7f848c0ded9dcca6337c02f03
    Author: josecerda1 <62906370+josecerda1@users.noreply.github.com>
    Date:   Wed Nov 17 22:41:16 2021 -0300
    
        comentarios y modificaciones minimas

--- interfaz.py
+++ interfaz.py
@@ -1,12 +1,17 @@
-
+#Lucho: Función encargada de presentar el tablero actualizado en pantalla
 def mostrar_tablero (tablero):
     fichas = ""
     numero = 1
+    contador=0
     for ficha in tablero :
+        if contador==4:
+            fichas+="\n "
+            contador=0
         if ficha[1] == 1 :
             fichas += ("["+ficha[0][0]+"]")
         else :
             
             fichas += "[" + str(numero) + "]"
+        contador+=1
         numero += 1
-    print(60*"*","\n\n","Fichas y posiciones :",fichas,"\n\n"+60*"*")
+    print(60*"*","\n\n","Fichas y posiciones :","\n",fichas,"\n\n"+60*"*")
