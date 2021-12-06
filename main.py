a1732c3dd3bedc8a467f4c6e81e6a148e7a3c1ec
lfaguilera <67346702+lfaguilera@users.noreply.github.com>
2021-12-06 16:49:21 -0300

    main con loop

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from orquestadores import orquestador
+from ranking import rankear
 
-def main ():
-    orquestador()
+def main (maximo=False):
+    continuar = True
+    while continuar and not maximo:
+        jugadores = orquestador()
+        continuar = rankear(jugadores)
 
 main()
